3c8949fa5b436bb59842e971072a6c481b17a9cf
Taehyeon Jake LEE <nayalth@gmail.com>
2017-10-12 13:50:37 +0900

    Update Java reducing indent

--- mySrc/Java/Level2/src/Baseball.java
+++ mySrc/Java/Level2/src/Baseball.java
@@ -8,54 +8,56 @@ import java.util.Scanner;
 
 
 public class Baseball {
-    
+
     private static List<Integer> getComputerNum() {
         List<Integer> computer = new ArrayList<Integer>();
-        while (computer.size() < 3) {
-            int newNum = (int)(Math.random() * 9) + 1;
-            if (!computer.contains(newNum)) {
-                computer.add(newNum);
-            }
+        for (int i = 0; i < 9; i++) {
+            computer.append(i+1);
         }
         Collections.shuffle(computer);
+        computer = computer.subList(0, 3);
         return computer;
     }
-    
+
     private static Map<String, Integer> countResult(Map<String, Integer> result,
             List<Integer> computer, String[] guessNum) {
         for (int i = 0; i < guessNum.length; i++) {
             int num = Integer.parseInt(guessNum[i]);
-            if (num == computer.get(i)) {
-                result.put("strike", result.get("strike") + 1);
-            } else if (computer.contains(num)) {
-                result.put("ball", result.get("ball") + 1);
-            }
+            result = checkStrikeNBall(result, computer, num);
         }
         return result;
     }
-    
+    private static Map<String, Integer> checkStrikeNBall(Map<String, Integer> result,
+            List<Integer> computer, int num) {
+        if (num == computer.get(i)) {
+            result.put("strike", result.get("strike") + 1);
+        } else if (computer.contains(num)) {
+            result.put("ball", result.get("ball") + 1);
+        }
+    }
+
     private static void printResult(Map<String, Integer> result) {
         String strResult = "";
         int strike = result.get("strike");
         int ball = result.get("ball");
-        
+
         if (strike > 0) strResult += strike + " 스트라이크 ";
         if (ball > 0) strResult += ball + " 볼";
-        
+
         if (strike == 0 && ball == 0) strResult += "낫싱";
-        
+
         System.out.println(strResult);
     }
-    
+
     public static void main(String[] args) {
-    	
+
         List<Integer> computer = getComputerNum();
         Map<String, Integer> result = new HashMap<>();
         result.put("strike", 0);
         result.put("ball", 0);
-        
+
         System.out.println("숫자야구게임");
-        
+
         Scanner sc = new Scanner(System.in);
         while (result.get("strike") != 3) {
             System.out.println("1부터 9사이의 숫자로 이루어진 세자리 숫자를 입력해주세요.");
